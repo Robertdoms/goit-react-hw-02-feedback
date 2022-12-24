b818085bec383cac429b0dcbdf1e552d10a6e413
RobertDoms <robertdoms90@gmail.com>
2022-12-24 23:24:03 +0200

    Create Notification.jsx

---
+++ src/components/Statistics/Notification.jsx
@@ -0,0 +1,8 @@
+import PropTypes from 'prop-types';
+const Notification = ({ message }) => {
+  return <p style={{ textAlign: 'center' }}>{message}</p>;
+};
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+export default Notification;
