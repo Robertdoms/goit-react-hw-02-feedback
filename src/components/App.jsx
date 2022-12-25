c8cc322de1f7369a600035461a0a7455ca0ace3d
RobertDoms <robertdoms90@gmail.com>
2022-12-25 12:23:50 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import FeedbackSection from './Section/Section';
+import Feedback from '../components/FeedbackOptions/FeedbackOptions';
+import Statistic from '../components/Statistics/Statistics';
+import Notification from '../components/Statistics/Notification';
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleBtn = option => {
+    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+  };
+
+  countTotalFeedback = () =>
+    Object.values(this.state).reduce((acc, value) => acc + value);
+}
+
+export default App;
