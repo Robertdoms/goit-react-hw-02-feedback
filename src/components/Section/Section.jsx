b7d76acc383bf7c429ec5bc661b3f2fc41dd00b3
RobertDoms <robertdoms90@gmail.com>
2022-12-25 12:23:53 +0200

    Update Section.jsx

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import { Section } from './Section.styled';
+
+const FeedbackSection = ({ title, children }) => {
+  return (
+    <Section>
+      <h2>{title}</h2>
+      {children}
+    </Section>
+  );
+};
+FeedbackSection.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default FeedbackSection;
