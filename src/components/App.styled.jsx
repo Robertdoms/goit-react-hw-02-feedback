93746d5891c2672944f3325403768d30d7593005
RobertDoms <robertdoms90@gmail.com>
2022-12-25 22:29:55 +0200

    Update App.styled.jsx

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  height: 100vh;
+  justify-content: center;
+  margin-top: 40px;
+  font-size: 40;
+  color: #010101;
+`;
