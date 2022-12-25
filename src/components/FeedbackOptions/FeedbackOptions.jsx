a0ddd124a5f92039e7493e6d36469b1bf15f7ea9
RobertDoms <robertdoms90@gmail.com>
2022-12-25 12:58:50 +0200

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,1 +1,5 @@
 import PropTypes from 'prop-types';
+import { Wrapper, BtnWrapper } from './FeedbackOptions.styled';
+
+const Feedback = ({ options, onFeedbackBtn }) => {};
+export default Feedback;
