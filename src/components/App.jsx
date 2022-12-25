081ec007d3fa521fdf84ecee773078697ecb7eba
RobertDoms <robertdoms90@gmail.com>
2022-12-25 12:55:27 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import FeedbackSection from './Section/Section';
-import Feedback from '../components/FeedbackOptions/FeedbackOptions';
+// import Feedback from '../components/FeedbackOptions/FeedbackOptions';
 import Statistic from '../components/Statistics/Statistics';
 import Notification from '../components/Statistics/Notification';
+import { Container } from './App.styled';
+
 class App extends Component {
   state = {
     good: 0,
@@ -16,6 +18,39 @@ class App extends Component {
 
   countTotalFeedback = () =>
     Object.values(this.state).reduce((acc, value) => acc + value);
+
+  countPositiveFeedbackPercentage = () =>
+    Math.round((this.state.good / this.countTotalFeedback()) * 100);
+
+  render() {
+    const totalFeedback = this.countTotalFeedback();
+    const feedBackPercentage = this.countPositiveFeedbackPercentage();
+    const { good, neutral, bad } = this.state;
+    return (
+      <Container>
+        {/* <FeedbackSection title="Please leave feedback">
+          <Feedback
+            options={Object.keys(this.state)}
+            onFeedbackBtn={this.handleBtn}
+          />
+        </FeedbackSection> */}
+
+        <FeedbackSection title="Statistic">
+          {!totalFeedback ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistic
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedback}
+              positivePercentage={feedBackPercentage ? feedBackPercentage : 0}
+            />
+          )}
+        </FeedbackSection>
+      </Container>
+    );
+  }
 }
 
 export default App;
