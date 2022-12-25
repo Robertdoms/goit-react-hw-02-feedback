b7cf8d051b3a5386a4c9133d1e544dbfe66d1081
RobertDoms <robertdoms90@gmail.com>
2022-12-25 12:55:30 +0200

    Update Statistics.styled.jsx

---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const StatisticWrapper = styled.div`
+  max-width: 300px;
+  margin: 0 auto;
+`;
+
+export const StatisticList = styled.ul`
+  margin: 0;
+  padding: 0;
+  margin-left: 20%;
+  list-style-type: none;
+  line-height: 1.3;
+`;
