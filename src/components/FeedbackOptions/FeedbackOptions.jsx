548e8da01aef2ae436d457354f97a5559529fb80
RobertDoms <robertdoms90@gmail.com>
2022-12-25 22:30:00 +0200

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,5 +1,26 @@
 import PropTypes from 'prop-types';
 import { Wrapper, BtnWrapper } from './FeedbackOptions.styled';
-
-const Feedback = ({ options, onFeedbackBtn }) => {};
+const Feedback = ({ options, onFeedbackBtn }) => {
+  return (
+    <Wrapper>
+      <BtnWrapper>
+        {options.map(option => (
+          <button
+            type="button"
+            onClick={() => {
+              onFeedbackBtn(option);
+            }}
+            key={option}
+          >
+            {option}
+          </button>
+        ))}
+      </BtnWrapper>
+    </Wrapper>
+  );
+};
+Feedback.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string.isRequired),
+  onFeedbackBtn: PropTypes.func.isRequired,
+};
 export default Feedback;
